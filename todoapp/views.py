ec33d56f6d5c16ac2d3adaf1d9922ae4507a4af5
Phumrapee Chaowanapricha <phumrapee.ch4352@gmail.com>
2025-04-16 14:03:27 +0700

    Modify views.py

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,73 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .models import Todo
+from .forms import TodoForm
 
+# Signup View
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('todo_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# Login View
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('todo_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+# Logout View
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# Display Todo List
+@login_required
+def todo_list(request):
+    todos = Todo.objects.filter(user=request.user)
+    return render(request, 'todo_list.html', {'todos': todos})
+
+# Add New Todo
+@login_required
+def add_todo(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.user = request.user
+            todo.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'add_todo.html', {'form': form})
+
+# Update Todo Status
+@login_required
+def update_todo_status(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id, user=request.user)
+    if request.method == 'POST':
+        new_status = request.POST.get('status')
+        if new_status in dict(Todo.STATUS_CHOICES):
+            todo.status = new_status
+            todo.save()
+    return redirect('todo_list')
+
+# Delete Todo
+@login_required
+def delete_todo(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id, user=request.user)
+    todo.delete()
+    return redirect('todo_list')

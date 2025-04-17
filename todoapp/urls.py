1275c21beaaacb41c5e7c6c442d8e5cdcd878f7d
Phumrapee Chaowanapricha <phumrapee.ch4352@gmail.com>
2025-04-17 16:56:41 +0700

    Update urls.py

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
+    path('', views.todo_list, name='todo_list'),
+    path('add/', views.add_todo, name='add_todo'),
+    path('update/<int:todo_id>/', views.update_todo_status, name='update_todo'),
+    path('delete/<int:todo_id>/', views.delete_todo, name='delete_todo'),
 ]
